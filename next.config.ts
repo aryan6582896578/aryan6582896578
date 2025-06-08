8d746e232ec4565d573b22c616cc5858154e66b2
Aryan <aryanmovalia@proton.me>
2025-06-08 14:43:24 +0530

    base 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  distDir: 'build',
+  output: "export",
 };
 
 export default nextConfig;
